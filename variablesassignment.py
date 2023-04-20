b392863ceb92b35d6e55a09713beb36df6df97c2
Tushaar00007 <111433153+Tushaar00007@users.noreply.github.com>
2023-04-20 13:47:26 +0530

    Add files via upload

---
+++ variablesassignment.py
@@ -0,0 +1,46 @@
+# Q1) write all conventions been followed by declaring a variable
+
+# string
+var1 = "one"
+# integer
+var2 = 5
+# floating number
+var3 = 525.36
+
+print(var1)
+print(var2)
+print(var3)
+
+# Q2) What will happen if we declare a restricted keyword as a variable
+# If we declare a restricted keyword as a variable in Python, we will get a syntax error when we try to run the program.
+# # In Python, certain words are reserved for specific purposes and cannot be used as variable names. These reserved words are called "keywords", and attempting to use them as variable names will result in a syntax error.
+# for example
+# pass = 54
+# print(pass )
+
+# Q3) Can we actually declare a string as a variable name?
+# ans-
+
+# new = "tushaar"
+# print(new)
+# tushaar = "hello world"
+# print(tushaar)
+
+# Q4) Is it possible for us to declare “_” as a variable? If so, then write an example of it
+# ans-yes it is possible to declare "_" as a variable because in python "_" it is a valid identifier and can be used as a variable name
+
+# _ = "hello world"
+# print(_)
+
+
+# Q5) Using an example, explain how the variables in python are dynamic in nature
+
+# ans - here you can see the example ,that one variable is redeclared again and again with differnet datatype
+# var = "Hitman"
+# print(var)
+
+# var = 25
+# print(var)
+
+# var = 45.245
+# print(var)
