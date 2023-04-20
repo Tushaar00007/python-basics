3b89b007ecf531b7c8f15915a04198956f632ca4
Tushaar00007 <111433153+Tushaar00007@users.noreply.github.com>
2023-04-20 23:19:43 +0530

    Add files via upload
    string function in python.

---
+++ string.py
@@ -0,0 +1,61 @@
+
+# Q1). How can we store a s1ngle quote (‘) as a str1ng 1n a var1able?
+
+# var = 'hello world'
+# print(var)
+
+# Q2). Refer the below var1able:
+# x = ‘a(
+# Here, 1s x a character type or str1ng type var1able? Support your answer w1th an explanat1on.
+# ans - x = 'a' is a string.
+
+# Q3.Apply the following functions type variable:'Welcome to python foundation course
+# 1.find()
+# 2.count()
+# 3.len(0)
+# 4.Concatenat1on
+
+# si = "Welcome to python foundation course"
+# sii="hello hitman"
+# print(si.find('to'))
+# print(si.count('p'))
+# print(len(si))
+# concat=si+" " +sii
+# print(concat)
+
+
+# Q4Q For the var1able: wor = ‘PanaJ1@T12256(
+# Calculate the follow1ng:
+#       (a) Total number of alphabets in lowercase
+#       (bTotal number of alphabets in uppercase
+#       (c) Total number of numer1cal in string
+
+# wor = 'PanaJ1@12256'
+# new = ''
+# count1 = 0
+# count2 = 0
+# count3 = 0
+
+# for a in wor:
+#     if (a.isupper()) == True:
+#         count1 += 1
+#         new += (a.lower())
+
+#     elif (a.islower()) == True:
+#         count2 += 1
+#         new += (a.upper())
+
+# print("uppercase:-", count1)
+# print("lowercase:-", count2)
+
+# wor = 'PanaJ1@T//56'
+
+# print(len(wor))
+
+
+# Q5 Wr1te a code to store a numer1cal value variable then convert i str1ng.
+# num = 10
+# convert = str(num)
+# print("conversion of numerical into string", type(convert))
+
+
